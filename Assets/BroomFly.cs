bddbb05d71be241bff86a1ea281d81e8f4fd39ab
Hannah-Sloan-ASE <sloanhannahava@gmail.com>
2020-03-03 12:05:39 -0700

    Perlin Noise and broom

---
+++ Assets/BroomFly.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BroomFly : MonoBehaviour
+{
+    public float force = 1;
+
+    void Update()
+    {
+        Vector3 direction = PerlinTest.Instance.GetValue(transform.position.x, transform.position.y);
+        direction.x = Mathf.Lerp(-1, 1, direction.x);
+        direction.y = Mathf.Lerp(-1, 1, direction.y);
+        direction.z = Mathf.Lerp(-1, 1, direction.z);
+        GetComponent<Rigidbody>().velocity = direction.normalized * force;
+    }
+}
---
+++ Assets/PerlinTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerlinTest : Singleton<PerlinTest>
+{
+    public int texXWidth = 153;
+    public int texYWidth = 55;
+
+    [Range(0,1)]
+    public float xScale = 1;
+    [Range(0, 1)]
+    public float yScale = 1;
+
+    public float xOffsetRed = 0;
+    public float yOffsetRed = 0;
+    public float xOffsetGreen = 0;
+    public float yOffsetGreen = 0;
+    public float xOffsetBlue = 0;
+    public float yOffsetBlue = 0;
+
+    [Range(0,1)]
+    public float scrollSpeed = 1;
+
+    [ContextMenu("Test")]
+    public void Update()
+    {
+        xOffsetRed += Time.deltaTime * Random.Range(0f,4) * scrollSpeed;
+        yOffsetRed += Time.deltaTime * Random.Range(0f, 4) * scrollSpeed;
+        xOffsetGreen += Time.deltaTime * Random.Range(0f, 4) * scrollSpeed;
+        yOffsetGreen += Time.deltaTime * Random.Range(0f, 4) * scrollSpeed;
+        xOffsetBlue += Time.deltaTime * Random.Range(0f, 4) * scrollSpeed;
+        yOffsetBlue += Time.deltaTime * Random.Range(0f, 4) * scrollSpeed;
+
+        //UpdateTexture();
+    }
+
+    public Vector3 GetValue(float i, float j)
+    {
+        var red = Mathf.PerlinNoise((i * xScale) + xOffsetRed, (j * yScale) + yOffsetRed);
+        var green = Mathf.PerlinNoise((i * xScale) + xOffsetGreen, (j * yScale) + yOffsetGreen);
+        var blue = Mathf.PerlinNoise((i * xScale) + xOffsetBlue, (j * yScale) + yOffsetBlue);
+        return new Vector3(red, green, blue);
+    }
+
+    public void UpdateTexture() 
+    {
+        Texture2D noiseTex = new Texture2D(texXWidth, texYWidth);
+        noiseTex.filterMode = FilterMode.Point;
+        noiseTex.wrapMode = TextureWrapMode.Clamp;
+        for (int i = 0; i < texXWidth; i++)
+        {
+            for (int j = 0; j < texYWidth; j++)
+            {
+                Vector3 color = GetValue(i, j);
+                noiseTex.SetPixel(i, j, new Color(color.x, color.y, color.z));
+            }
+        }
+        noiseTex.Apply();
+
+        GetComponent<Renderer>().material.mainTexture = noiseTex;
+    }
+}
